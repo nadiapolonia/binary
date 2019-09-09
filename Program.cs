da50e799da9fa7136b9448d1178739b78a9af46a
Nadia Murchison <nadiamurchison@gmail.com>
2019-09-09 18:57:14 -0400

    basic algorithm made, needs response in console

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Array.Sort(numbers);
+            Console.WriteLine("Hello World!");
+
+        }
+
+
+
+        // static void NumberArray(int[] number) {
+        //     int[] sequence = Enumberable.Range(1,1000).ToArray();
+        // }
+
+        public static int BinarySearch(int[] numbers, int item)
+        {
+            return BinarySearch(numbers, 0, numbers.Length - 1, item);
+        }
+
+        public static int BinarySearch(int[] numbers, int lower, int higher, int item)
+        {
+            if (lower <= higher)
+            {
+                int guess = lower + (higher - lower) / 2;
+
+                if (numbers[guess] == item)
+                    return guess;
+
+                if (numbers[guess] > item)
+                    return BinarySearch(numbers, guess + 1, higher, item);
+
+            }
+            return -1;
+        }
+
+    }
+}
